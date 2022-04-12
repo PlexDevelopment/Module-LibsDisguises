56d1d97c970e23ee8e289561c6b80a2dfd7e9deb
Telesphoreo <me@telesphoreo.me>
2022-04-12 16:46:25 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,13 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.22")
     annotationProcessor("org.projectlombok:lombok:1.18.22")
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
-    compileOnly("dev.plex:Plex:1.0")
+    compileOnly("dev.plex:Plex:1.0.1-SNAPSHOT")
     implementation("LibsDisguises:LibsDisguises:10.0.28")
 }
 
 group = "dev.plex"
 version = "1.0"
-description = "Plex-LibsDisguises"
+description = "Module-LibsDisguises"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
