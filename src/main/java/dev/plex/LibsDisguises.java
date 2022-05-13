b3fb0f01f3234b8165f34fa72fa33a02770be88c
Telesphoreo <me@telesphoreo.me>
2022-05-12 22:17:50 -0500

    Add check to make sure LibsDisguises is actually on the server

--- src/main/java/dev/plex/LibsDisguises.java
+++ src/main/java/dev/plex/LibsDisguises.java
@@ -4,14 +4,22 @@ import dev.plex.command.DisguiseToggleCMD;
 import dev.plex.command.UndisguiseAllCMD;
 import dev.plex.listener.DisguiseListener;
 import dev.plex.module.PlexModule;
+import dev.plex.util.PlexLog;
+import org.bukkit.Bukkit;
 
 public class LibsDisguises extends PlexModule
 {
     public static boolean enabled = true;
     DisguiseListener disguiseListener;
+
     @Override
     public void enable()
     {
+        if (!Bukkit.getPluginManager().isPluginEnabled("LibsDisguises"))
+        {
+            PlexLog.error("The Plex-LibsDisguises module requires the LibsDisguises plugin to work.");
+            return;
+        }
         registerCommand(new DisguiseToggleCMD());
         registerCommand(new UndisguiseAllCMD());
         registerListener(new DisguiseListener());
