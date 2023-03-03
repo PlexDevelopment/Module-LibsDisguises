572c8cd2eb4b120b49ed9277e6d99cc13c892648
Telesphoreo <me@telesphoreo.me>
2023-03-02 23:16:38 -0600

    Update dependencies and Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,9 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.24")
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
-    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
     compileOnly("dev.plex:server:1.3-SNAPSHOT")
     implementation("LibsDisguises:LibsDisguises:10.0.28")
 }
